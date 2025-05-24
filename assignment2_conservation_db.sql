8d46805e67d220ed2af5b9f1d77053c1306c5423
ajkn1904 <ajknishat@gmail.com>
2025-05-25 00:23:38 +0600

    problem 1 added

--- assignment2_conservation_db.sql
+++ assignment2_conservation_db.sql
@@ -37,10 +37,15 @@ CREATE TABLE sightings(
     notes VARCHAR(500)
 );
 
-INSERT into sightings(location, sighting_time, notes) VALUES
-('Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
-('Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
-('Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
-('Snowfall Pass', '2024-05-18 18:30:00', NULL); 
+INSERT into sightings(species_id, ranger_id, location, sighting_time, notes) VALUES
+(1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
+(2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
+(3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
+(1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL); 
 
 SELECT * from sightings;
+
+
+
+--Problem 1: 1️⃣ Register a new ranger with provided data with name = 'Derek Fox' and region = 'Coastal Plains'
+INSERT into rangers(name, region) VALUES('Derek Fox', 'Coastal Plains');
